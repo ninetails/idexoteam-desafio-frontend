864b12db6ef9cb2977e5698d74f61948dc06ba61
Carlos Kazuo <ninetails.nine@gmail.com>
2018-02-06 22:50:05 -0200

    test(app): Fixed App.test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { StyleRoot } from 'radium';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<StyleRoot><App /></StyleRoot>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
