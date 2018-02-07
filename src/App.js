6a10bc43c4184d7ccaadf7cecd45e1af49f66a11
Carlos Kazuo <ninetails.nine@gmail.com>
2018-02-06 22:48:47 -0200

    feat(css): Migrated styles to radium

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
 import React from 'react';
+import Radium, { keyframes } from 'radium';
 import logo from './logo.svg';
-import './App.css';
+
+const logoSpin = keyframes({
+  from: {
+    transform: 'rotate(0deg)'
+  },
+  to: {
+    transform: 'rotate(360deg)'
+  }
+}, 'spin');
+
+const styles = {
+  app: {
+    textAlign: 'center',
+    header: {
+      backgroundColor: '#222',
+      color: 'white',
+      height: '150px',
+      padding: '20px',
+    },
+    intro: {
+      fontSize: 'large'
+    },
+    logo: {
+      animation: 'x infinite 20s linear',
+      animationName: logoSpin,
+      height: '80px'
+    },
+    title: {
+      fontSize: '1.5em'
+    }
+  }
+};
+
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
+  <div style={styles.app}>
+    <header style={styles.app.header}>
+      <img src={logo} style={styles.app.logo} alt="logo" />
+      <h1 style={styles.app.title}>Welcome to React</h1>
     </header>
-    <p className="App-intro">
+    <p style={styles.app.intro}>
       To get started, edit <code>src/App.js</code> and save to reload.
     </p>
   </div>
 );
 
-export default App;
+export default Radium(App);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { StyleRoot } from 'radium';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<StyleRoot><App /></StyleRoot>, document.getElementById('root'));
 registerServiceWorker();
