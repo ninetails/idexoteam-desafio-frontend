ff04a0bd3a66a1158edd394105a509279d166f6d
Carlos Kazuo <ninetails.nine@gmail.com>
2018-02-09 15:09:20 -0200

    fix(app): Reset application to a blank one

--- src/App.js
+++ src/App.js
@@ -1,50 +1,9 @@
-import React from 'react';
-import Radium, { keyframes } from 'radium';
-import logo from './logo.svg';
-
-const logoSpin = keyframes({
-  from: {
-    transform: 'rotate(0deg)'
-  },
-  to: {
-    transform: 'rotate(360deg)'
-  }
-}, 'spin');
-
-const styles = {
-  app: {
-    textAlign: 'center',
-    header: {
-      backgroundColor: '#222',
-      color: 'white',
-      height: '150px',
-      padding: '20px',
-    },
-    intro: {
-      fontSize: 'large'
-    },
-    logo: {
-      animation: 'x infinite 20s linear',
-      animationName: logoSpin,
-      height: '80px'
-    },
-    title: {
-      fontSize: '1.5em'
-    }
-  }
-};
-
+import React, { Fragment } from 'react';
 
 const App = () => (
-  <div style={styles.app}>
-    <header style={styles.app.header}>
-      <img src={logo} style={styles.app.logo} alt="logo" />
-      <h1 style={styles.app.title}>Welcome to React</h1>
-    </header>
-    <p style={styles.app.intro}>
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </div>
+  <Fragment>
+    Blank app
+  </Fragment>
 );
 
-export default Radium(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { StyleRoot } from 'radium';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<StyleRoot><App /></StyleRoot>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
