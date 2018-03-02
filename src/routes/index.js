80aaedb48b4ed5a2b10cea3411458dfd8cf335ef
Carlos Kazuo <ninetails.nine@gmail.com>
2018-03-02 01:52:07 -0300

    fix(background): Added blank space for mobile to maintain background position

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Switch
 } from 'react-router-dom';
+import Radium from 'radium';
 import DocumentTitle from 'react-document-title';
 import MarvelComicListContainer from '../containers/MarvelComicListContainer';
 import ComicDetailsPage from '../pages/ComicDetailsPage';
 import StarkDexContainer from '../organisms/StarkDexContainer';
+
+const blankDetailsStyle = {
+  flex: '1 0 100%',
+  width: '100vw',
+  '@media (min-width: 768px)': {
+    display: 'none'
+  }
+};
+
+const BlankMobilePage = Radium(props => <div {...props} style={blankDetailsStyle} />);
 
 export default () => (
   <DocumentTitle title="Starkdex">
@@ -16,7 +28,10 @@ export default () => (
         component={props => (
           <StarkDexContainer {...props}>
             <MarvelComicListContainer {...props} />
-            <Route path="/:comicId" component={ComicDetailsPage} />
+            <Switch>
+              <Route path="/:comicId" component={ComicDetailsPage} />
+              <Route component={BlankMobilePage} />
+            </Switch>
           </StarkDexContainer>
         )}
       />
